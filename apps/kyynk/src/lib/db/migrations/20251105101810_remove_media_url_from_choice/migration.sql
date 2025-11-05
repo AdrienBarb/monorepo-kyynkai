1eff3997bc3af50ca15911d6861de2f6f3f847c5
Adrien Barbier <adrien-barbier@hotmail.fr>
2025-11-05 11:23:03 +0100

    go

---
+++ apps/kyynk/src/lib/db/migrations/20251105101810_remove_media_url_from_choice/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `mediaUrl` on the `FantasyChoice` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FantasyChoice" DROP COLUMN "mediaUrl";
